a6865097fcabbc23afa03a8b783654986e188438
amarangonihengling <ahengling@azi.com.br>
2017-11-16 14:53:34 -0300

    Feature: adding a new method CrudRepository.findAllAndCount which returns the records that matches the criteria and the total of elements that matches the specified criteria on database.

--- src/repositories/CrudRepository.js
+++ src/repositories/CrudRepository.js
@@ -7,6 +7,24 @@ export default class CrudRepository {
   findAll(criteria) {
     return this.model.find(criteria);
   }
+  
+  findAllAndCount(criteria) {
+    let totalElements = 0;
+    let records = [];
+    
+    return new Promise((resolve, reject) => {
+      this.findAll(criteria)
+        .then((result) => {
+          records = result;
+          return this.model.count(criteria);
+        })
+        .then((count) => {
+          totalElements = count;
+          resolve({ records, totalElements });
+        })
+        .catch(err => reject(err));
+    });
+  }
 
   findAllWithDeps(criteria, deps) {
     return this.model.find(criteria).populate(deps);
